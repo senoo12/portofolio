ddab7bb54689c449c779cdce5a04587511abdcc7
Senoaji Pamungkas <76572178+senoo12@users.noreply.github.com>
2023-03-12 08:34:31 +0700

    Add files via upload

---
+++ seno/seno.js
@@ -0,0 +1,11 @@
+function toggleNav() {
+  var x = document.getElementsByTagName("nav")[0];
+  if (x.className === "bottomNav") {
+    x.className += " openNav";
+  } else {
+    x.className = "bottomNav";
+  }
+}
+document.querySelector(".hamburger").addEventListener("click", toggleNav);
+
+$(".dropdown-toggle").dropdown();
